10c757f5c0325fdc12d5ab8b95524b63d7a02abb
Carlita <info@cwiggan.com>
2012-06-04 13:39:20 -0400

    virgin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a2ec95ebb677cb187057e9e85e240c019bb82e61d09d4d720b42e1fb30dd530fcbc1188529a34097f1968acc5cf9321cea61253e1b1492b1e1d5e7e7a80b0313'
